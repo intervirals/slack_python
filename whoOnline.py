e875d96d3680810ef6286fdacbc935393501efaa
intervirals <intervirals@gmail.com>
2016-08-20 21:46:38 +1000

    Update whoOnline.py

--- whoOnline.py
+++ whoOnline.py
@@ -2,4 +2,12 @@ from slacker import Slacker
 
 slack = Slacker('xoxp-70997678455-71005628514-71312901170-f87b8ff08c') #intervirals_testing slack token generated
 
-slack.chat.post_message('#general', 'Hello fellow slackerss! :D')
+# Send a message to #general channel
+slack.chat.post_message('#general', 'Hello fellow slackersssss :D!')
+
+# Get users list
+response = slack.users.list()
+users = response.body['members']
+
+# Upload a file
+slack.files.upload('hello.txt')
