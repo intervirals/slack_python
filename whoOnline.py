3554117b7c192600de9fc50ac9e4bd94cacaf169
intervirals <intervirals@gmail.com>
2016-08-20 21:35:26 +1000

    Update whoOnline.py

--- whoOnline.py
+++ whoOnline.py
@@ -1,5 +1,5 @@
 from slacker import Slacker
 
-slack = Slacker('<xoxp-70997678455-71005628514-71312901170-f87b8ff08c>') #intervirals_testing slack token generated
+slack = Slacker('xoxp-70997678455-71005628514-71312901170-f87b8ff08c') #intervirals_testing slack token generated
 
 slack.chat.post_message('#general', 'Hello fellow slackerss! :D')
